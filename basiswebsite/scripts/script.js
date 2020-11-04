b6e7973800e6482dd292f7a867eb2c1c7f773b3c
omarkhalifa <omarkhalifa14@gmail.com>
2020-11-04 17:28:36 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,61 +1,24 @@
-// index
+// carroussel categorieen
 
-    // Verander attribute positie naar 0 voor alle .scroll elementen
-	const scrollContainers = document.querySelectorAll(".scroll");
-	for (let index = 0; index < scrollContainers.length; index++) {
-		const element = scrollContainers[index];
-		element.setAttribute("pos", 0);	
-	}
+var i = 0; //startpunt
+var images = []
 
-	// Click op linker pijltje, 
-	// get the slider element en verlaag positie atribuut met 1 als het kan
-	// and call scrollToChild()
-	function scrollToLeft(id) {
-		var parent = document.querySelector('#' + id);
-		console.log(parent);
-		var pos = parent.getAttribute("pos");
-		pos = parseInt(pos);
-		if (pos > 0) {
-			parent.setAttribute("pos", pos - 1);
-			scrollToChild(parent);
-		}
-	}
+// afbeeldingen
+images[0] = 'images/cat/boeken.jpg';
+images[1] = 'images/cat/electronica.jpg';
+images[2] = 'images/cat/speelgoed.jpg';
+images[3] = 'images/cat/wonen.jpg';
+images[4] = 'images/cat/baby.jpg';
+images[5] = 'images/cat/koken.jpg';
+images[6] = 'images/cat/beauty.jpg';
+images[7] = 'images/cat/sport.jpg';
 
-	// Click op recht pijltje 
-	// get slider element en verhoog positie atribuut met 1 als het kan
-	// and call scrollToChild()
-	function scrollToRight(id) {
-		var parent = document.querySelector('#' + id);
-		console.log(parent);
-		var pos = parent.getAttribute("pos");
-		pos = parseInt(pos);
-		if (pos < parent.children.length - 2) {
-			parent.setAttribute("pos", pos + 1);
-			scrollToChild(parent);
-		}
-	}
 
-	// update slider element naar juiste positie
-	function scrollToChild(parent) {
-		const pos = parent.getAttribute("pos");
-		const child = parent.children[pos];
-		child.parentNode.scrollLeft = child.offsetLeft;
-	}
+// Functie afbeeldingen schuiven
+document.slide.src = images[i];
 
-// product
-
-    // Dropdown info en Product titel verzamelen van de product pagina
-	const dropdown = document.querySelector("select#option");
-	const productTitle = document.querySelector("h1");
-
-	// switched id element naar actief 
-	function toggleActive(id) {
-		const favoriet = document.querySelector("#" + id);
-		favoriet.toggleAttribute("active");
-	}
-
-	// listener voor verandering van het dropdown element
-	dropdown.addEventListener("change", function () {
-		var name = this.target;
-		productTitle.textContent = this.value;
-	});
+if (i < images.lenght - 1){
+	i++;
+} else{ }
+}
+]
